61d871e0fe1aba84e6a058664104a87a97d81638
nirob360 <mdminarulislam9400@gmail.com>
2022-02-12 20:57:59 +0600

    first commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 import { GoLocation } from "react-icons/go";
 
 const Sidebar = () => {
@@ -30,30 +29,6 @@ const Sidebar = () => {
         Web Designer & Developer
       </p>
 
-      <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full ">
-        <a
-          target="_blank"
-          href="https://www.youtube.com/channel/UCb0zct7IaK3OS5ZqwIXdReQ/featured"
-          aria-label="Youtube"
-        >
-          <AiFillYoutube className="w-8 h-8 cursor-pointer" />
-        </a>
-        <a
-          target="_blank"
-          href="https://www.linkedin.com/in/sanvi-rohoman-99752622a/"
-          aria-label="Linkdin"
-        >
-          <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
-        </a>
-        <a
-          target="_blank"
-          href="https://github.com/sungreatit "
-          aria-label="github"
-        >
-          <AiFillGithub className="w-8 h-8 cursor-pointer" />
-        </a>
-      </div>
-
       <div
         className="py-4 my-5 bg-gray-200 dark:bg-dark-200 dark:bg-black-500"
         style={{ marginLeft: "-1rem", marginRight: "-1rem" }}
@@ -61,15 +36,6 @@ const Sidebar = () => {
         <div className="flex items-center justify-center">
           <GoLocation className="mr-2" /> <span>Dhaka Bangladesh </span>
         </div>
-        <p
-          className="my-2 cursor-pointer"
-          onClick={() => window.open("mailto:code.sumax@gmail.com")}
-        >
-          developerbd.org.freelance@gmail.com
-        </p>
-        <p className="my-2">
-          <a href="tel:01788420417">01788420417</a>
-        </p>
       </div>
 
       <button
